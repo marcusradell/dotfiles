81a14437c2d91757e7b97783581626210c8fdfe9
Marcus Rådell <marcus@radell.net>
2021-10-08 16:41:59 +0200

    Update manual_install.sh

--- manual_install.sh
+++ manual_install.sh
@@ -5,3 +5,9 @@ open https://ohmyz.sh/#install
 open https://bitwarden.com/download/
 open https://github.com/alacritty/alacritty/releases/
 open https://apps.apple.com/us/app/magnet/id441258766
+open https://discord.com/download
+open https://www.around.co/
+open https://www.microsoft.com/en-us/microsoft-teams/download-app
+open https://apps.apple.com/app/slack/id803453959
+open https://www.microsoft.com/en-us/microsoft-teams/download-app#desktopAppDownloadregion
+open https://zoom.us/support/download?os=mac
